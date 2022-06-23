0a009cf6dcc291a92bd9ec88ed890b047809ff53
rahiyansafz <rahiyansafin@gmail.com>
2022-06-24 01:19:03 +0600

    Caching Implemented

--- Hotels.Models/Models/Booking.cs
+++ Hotels.Models/Models/Booking.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Hotels.Models.Models.Auth;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hotels.Models.Models;
 
@@ -15,4 +16,8 @@ public class Booking
     [ForeignKey(nameof(HotelId))]
     public int HotelId { get; set; }
     public Hotel Hotel { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public int UserId { get; set; }
+    public User User { get; set; }
 }
